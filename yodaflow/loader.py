7b5f225d33f22ce0de9a9a943139831b3c56f6c3
adryan <adryanftaborda@gmail.com>
2025-07-18 22:40:34 -0300

    feat: add loader function for workflow

---
+++ yodaflow/loader.py
@@ -0,0 +1,17 @@
+# used for parsing and validating yamls
+
+import yaml
+from typing import Dict, Any
+from yodaflow.exceptions import WorkflowValidationError
+
+def load_workflow(yaml_path: str) -> Dict[str, Any]:
+    try:
+        with open(yaml_path, 'r') as file:
+            workflow = yaml.safe_load(file)
+            if not isinstance(workflow, dict):
+                raise WorkflowValidationError("...")
+        return workflow
+    except FileNotFoundError:
+        raise WorkflowValidationError("...")
+    except yaml.YAMLError as e:
+        raise WorkflowValidationError(e)
